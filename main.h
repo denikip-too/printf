361b6a6126fa89eef16653137836be67dcea388c
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-10-04 16:18:22 -0700

    main.h

--- main.h
+++ main.h
@@ -4,7 +4,15 @@
 #include <stdarg.h>
 #include <stdlib.h>
 
+typedef struct op
+{
+	char *op;
+	int (*f)(va_list);
+} op_t;
+
 int _printf(const char *format, ...);
 int _putchar(char c);
+int op_c(va_list c);
+int op_s(va_list s);
 
 #endif
