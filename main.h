b27977955553177a780a245553a1893ca31a325c
kiprotich-denis <kiprotichdenis95@gmail.com>
2021-10-05 12:26:43 -0700

    main.h

--- main.h
+++ main.h
@@ -15,11 +15,12 @@ typedef struct op
 	int (*f)(va_list);
 } op_t;
 
+int (*specifier(const char *format))(va_list);
 int _printf(const char *format, ...);
 int _putchar(char c);
-int op_c(va_list c);
-int op_s(va_list s);
-int op_d(va_list d);
-int op_i(va_list i);
+int op_c(va_list args);
+int op_s(va_list args);
+int op_d(va_list args);
+int op_i(va_list args);
 
 #endif
